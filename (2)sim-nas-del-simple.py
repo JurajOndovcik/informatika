6662b8ba0e3d7542beddd8df1692c3ebff2417d4
Juraj Ondovčík <109098739+JurajOndovcik@users.noreply.github.com>
2025-05-21 20:00:27 +0200

    simple 2

---
+++ (2)sim-nas-del-simple.py
@@ -0,0 +1,29 @@
+
+A = int(input('Vlož A: '))
+B = int(input('Vlož B: '))
+
+možnosť = input(' \nVyber možnosť:\n[1] Simulácia násobenia\n[2] Simulácia delenia\nMožnosť: ')
+if možnosť == '1':
+    print(' \nSimulácia násobenia:')
+    tempB = ''
+    for i in range(B):
+        tempB += str(A)
+        if i < B - 1:
+            tempB += ' + '
+    print(f'{A} * {B} = {tempB} = {A * B}')
+elif možnosť == '2':
+    print(' \nSimulácia delenia:')
+    pocet = 0
+    if A < B:
+        print(f'{A} : {B} = {pocet} (zv.) {A % B}')
+    elif A == B:
+        print(f'{A} : {B} = 1 (zv.) 0')
+    else:
+        tempA = A
+        while tempA >= B:
+            print(f'{tempA} - {B} = {tempA - B}')
+            tempA -= B
+            pocet += 1
+        print(f'{A} : {B} = {pocet} (zv.) {tempA}')
+else:
+    print('Zlá možnosť')
